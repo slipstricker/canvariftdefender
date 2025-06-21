d168997225eb62196d433038f143400e267001dc
slipstricker <48421749+slipstricker@users.noreply.github.com>
2025-06-21 15:33:24 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -2569,9 +2569,8 @@ const App: React.FC = () => {
                  <button onClick={handleViewDebugPanel} className={`${commonButtonClass} mt-4 w-64 md:w-80`}>
                     Debug
                 </button>
-                <div className="mt-auto pt-6 text-center text-xs text-gray-400">
+                <div className="mt-auto pt-1 text-center text-xs text-gray-400">
                   <p className="mb-1">Controles: A/D para mover, Espaço para pular, {playerRef.current.hasDashSkill && getPermanentSkillLevel(SKILL_ID_DASH) > 0 ? "Shift para Dash, " : ""}Mouse para mirar e atirar. P para Pausar.</p>
-                  <p>&copy; {new Date().getFullYear()} Pixel Rift Defenders. Uma Aventura Cósmica de IA.</p>
                 </div>
             </div>
         )}
